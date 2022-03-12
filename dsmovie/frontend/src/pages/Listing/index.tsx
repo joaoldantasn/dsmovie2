5c1ea0d1a727465c375cbf370003e33919e1c081
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2022-03-12 16:51:52 -0300

    MovieCard

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -1,13 +1,51 @@
-import MovieScore from "components/MovieScore";
-import MovieStars from "components/MovieStars";
+import MovieCard from "components/MovieCard";
 import Pagination from  "components/Pagination";
 
 function Listing(){
   return(
     <>
        <Pagination/>
-       <MovieStars/>
-       <MovieScore/>
+
+       <div className = "container">
+          <div className = "row">
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+            <div className = "col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard/>
+            </div>
+          </div>
+       </div>
     </>
   );
 }
