0f6dafc5a47025a33a0412cab2f79d419ff9b8c7
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2022-03-13 21:54:33 -0300

    useNavigate

--- dsmovie/frontend/src/components/FormCard/index.tsx
+++ dsmovie/frontend/src/components/FormCard/index.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
 import { validateEmail } from 'utils/validate';
@@ -12,6 +12,7 @@ type Props = {
 
 function FormCard({ movieId }: Props) {
 
+  const navigate = useNavigate();
   const [movie, setMovie] = useState<Movie>();
 
   useEffect(() => {
@@ -41,7 +42,7 @@ function FormCard({ movieId }: Props) {
       }
     }
     axios(config).then(response => {
-      console.log(response.data);
+      navigate("/");
     });
   }
 
